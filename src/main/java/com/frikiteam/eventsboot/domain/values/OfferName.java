5186596738859c4df96a90361803df1de2299c6e
Dani237 <54338925+Dani237@users.noreply.github.com>
2022-05-07 02:38:42 -0500

    Feat: OfferName value object added

---
+++ src/main/java/com/frikiteam/eventsboot/domain/values/OfferName.java
@@ -0,0 +1,52 @@
+package com.frikiteam.eventsboot.domain.values;
+
+import lombok.Value;
+import com.frikiteam.eventsboot.command.application.notification.Notification;
+import com.frikiteam.eventsboot.command.application.notification.Result;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Value
+public class OfferName {
+    @Column(name = "name_offer_name")
+    private String name;
+    @Column(name = "reference_offer_name")
+    private String reference;
+    private final static int MAX_LENGTH = 100;
+
+    private OfferName(String name, String reference) {
+        this.name = name;
+        this.reference = reference;
+    }
+
+    protected OfferName() {
+        this.name = "";
+        this.reference = "";
+    }
+
+    public static Result<OfferName, Notification> create(String name, String reference) {
+        Notification notification = new Notification();
+        name = name == null ? "" : name.trim();
+        reference = reference == null ? "" : reference.trim();
+        if (name.isEmpty()) {
+            notification.addError("name is required", null);
+            return Result.failure(notification);
+        }
+        if (reference.isEmpty()) {
+            notification.addError("reference is required", null);
+            return Result.failure(notification);
+        }
+        if (name.length() > MAX_LENGTH) {
+            notification.addError("The maximum length of a name is " + MAX_LENGTH + " characters including spaces", null);
+        }
+        if (reference.length() > MAX_LENGTH) {
+            notification.addError("The maximum length of a reference is " + MAX_LENGTH + " characters including spaces", null);
+        }
+        if (notification.hasErrors()) {
+            return Result.failure(notification);
+        }
+        return Result.success(new OfferName(name, reference));
+    }
+}
